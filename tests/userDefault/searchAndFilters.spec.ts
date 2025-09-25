58e6c291e1d8efc3cee670cc4ddd419a9505a963
Jaime Junior <jaimegenerali@gmail.com>
2025-09-25 09:01:54 -0300

    test: addind space to check sth

--- tests/userDefault/searchAndFilters.spec.ts
+++ tests/userDefault/searchAndFilters.spec.ts
@@ -2,6 +2,7 @@ import { test } from '../../support/index';
 
 test.use({ storageState: './playwright/.auth/userPadrao.json' });
 
+
 test.beforeEach(async ({ page }) => {
   await page.goto('/dashboard.html');
 });
